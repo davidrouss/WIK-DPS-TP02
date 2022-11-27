1b79bef5f5e42d4c5e6f1391a22f05e8fe3437d6
david roussat <roussatdavd.rd@gmaail.com>
2022-11-27 15:50:50 +0100

    fin

--- target/debug/deps/wik_dps_tp01-f34dc875eb85088a.d
+++ target/debug/deps/wik_dps_tp01-f34dc875eb85088a.d
@@ -1,5 +1,5 @@
-/home/david/Devops/target/debug/deps/wik_dps_tp01-f34dc875eb85088a: src/main.rs
+/home/david/WIK-DPS-TP02/target/debug/deps/wik_dps_tp01-f34dc875eb85088a: src/main.rs
 
-/home/david/Devops/target/debug/deps/wik_dps_tp01-f34dc875eb85088a.d: src/main.rs
+/home/david/WIK-DPS-TP02/target/debug/deps/wik_dps_tp01-f34dc875eb85088a.d: src/main.rs
 
 src/main.rs:
--- target/release/wik_dps_tp01.d
+++
@@ -1,1 +0,0 @@
-/home/hessman/Desktop/devops-22-23/wik-dps-tp01-correction-rust/target/release/wik_dps_tp01: /home/hessman/Desktop/devops-22-23/wik-dps-tp01-correction-rust/src/main.rs
